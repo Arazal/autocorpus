4baf76af99cf490a01c71a40221f5b0085d57d90
Adel <azalaradle@gmail.com>
2019-11-16 16:36:56 +0300

    Updates

---
+++ precache-manifest.a379f18c1780f9222dd604aee036f7fc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09983fdac83c21c954e07214214c23f0",
+    "url": "/autocorpus/index.html"
+  },
+  {
+    "revision": "b11f900b999c7e3ecbe3",
+    "url": "/autocorpus/static/css/main.f4a3454a.chunk.css"
+  },
+  {
+    "revision": "00d3821ad62642d2036a",
+    "url": "/autocorpus/static/js/2.aa67c30f.chunk.js"
+  },
+  {
+    "revision": "b11f900b999c7e3ecbe3",
+    "url": "/autocorpus/static/js/main.6643564a.chunk.js"
+  },
+  {
+    "revision": "d5c45a9c77c69f5089b5",
+    "url": "/autocorpus/static/js/runtime-main.aec1f07d.js"
+  },
+  {
+    "revision": "a023ed13ff5337ff4a76c10039efc5f5",
+    "url": "/autocorpus/static/media/3.a023ed13.png"
+  },
+  {
+    "revision": "3f02e26eb7764a4b76e3c992652b2dfd",
+    "url": "/autocorpus/static/media/light.3f02e26e.jpg"
+  },
+  {
+    "revision": "c13cec1a788d0093cc57aba2f72c9d26",
+    "url": "/autocorpus/static/media/redcar-wo-color.c13cec1a.jpg"
+  },
+  {
+    "revision": "68cd5f9a40df9891ba10b17f5dfadc58",
+    "url": "/autocorpus/static/media/vw.68cd5f9a.jpg"
+  }
+]);
